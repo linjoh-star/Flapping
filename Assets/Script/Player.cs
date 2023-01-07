06702354677564caa88a45fa74a1fd236b4f2e17
linjoh-star <linus.johansson.tk20@amerikanskagymnasiet.se>
2023-01-07 08:11:29 +0100

    småfixar + ljud

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -7,35 +7,38 @@ public class Player : MonoBehaviour
 {
     public Text txt_points;
     int points = 0;
-
     public GameObject gameover_panel;
+    public AudioClip lose;
 
     void Start()
     {
-        
+
     }
 
     void Update()
-    {
+    { 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(0, 7);
-            GetComponent<Animator>().Play("fly",0,0);
+            GetComponent<Animator>().Play("fly", 0, 0);
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.collider.tag == "Floor") {
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Floor")
+        {
             //Debug.Log("Game Over");
             Time.timeScale = 0;
-
             gameover_panel.SetActive(true);
+            GetComponent<AudioSource>().PlayOneShot(lose);
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Point"){
+        if (collision.tag == "Point")
+        {
             Debug.Log("+1 POINT");
             points++;
             txt_points.text = points.ToString();
